d9acc308e56f45a3396f625c93563ec61d9c09e0
Mansi Mande <mansimande15@gmail.com>
2025-04-20 21:09:30 +0530

    RMS-123 This is the test commit

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my first Github commit integrating with Jenkins");
+        System.out.println("This commit for feature into master");
+    }
+}
