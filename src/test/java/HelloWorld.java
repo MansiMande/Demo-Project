da2aa01574b7aa0195264f5a7c62bd02a9380c77
Mansi Mande <mansimande15@gmail.com>
2025-04-24 00:39:58 +0530

    Doing Jenkins pipeline failure checkin

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public staticcc void main(String[] args) {
         System.out.println("This is my first Github commit integrating with Jenkins");
         System.out.println("This commit for feature into master");
         System.out.println("This Demo is for git rebase");
