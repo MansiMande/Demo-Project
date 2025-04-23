e055c3f2c6c497db1a540b27e8493c8cb5d951cc
MansiMande <mansimande15@gmail.com>
2025-04-23 18:04:27 +0530

    checking jenkins correct build

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public staticccc void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("This is my first Github commit integrating with Jenkins");
         System.out.println("This commit for feature into master");
          System.out.println("This Demo is for git rebase");
